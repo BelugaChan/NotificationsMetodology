bec698b58c9a75210445f5c7fa5629fe663efdf0
BelugaChan <tuchnyy@list.ru>
2025-05-11 14:39:21 +0300

    Add new command interface
    Signed-off-by: BelugaChan <tuchnyy@list.ru>

---
+++ Interfaces/IUserBioCommandService.cs
@@ -0,0 +1,11 @@
+﻿using NotificationsService.Events;
+using NotificationsService.Models;
+
+namespace NotificationsService.Interfaces
+{
+    public interface IUserBioCommandService
+    {
+        Task AppendEventAsync<T>(T @event, UserNotification? currentState = null)
+            where T : Event;
+    }
+}
